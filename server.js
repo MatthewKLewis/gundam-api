4d4f093c677a2e3756ef8475f0f4ee6f15f78645
Matthew Lewis <matthew.knight.lewis@gmail.com>
2020-10-30 18:19:36 -0400

    server is ready and independent of the front end

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const port = 5225
 app.set('json spaces', 2)
 
 app.get('/', (req,res) => {
+    res.send(`
+        <p>Go to: </p> 
+        <a href="http://localhost:5225/gundam">this page</a> 
+        <p>for a random gundam</p>
+    `)
+})
+
+app.get('/gundam', (req,res) => {
     Gundam.findOne()
         .then(gundam => res.json(gundam))
         .catch(err => res.send(err))
