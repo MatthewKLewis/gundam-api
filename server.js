9d70645f597ee3c6dfa5976cb9c4550d5a8a26e0
Matthew Lewis <matthew.knight.lewis@gmail.com>
2020-11-05 10:20:15 -0500

    added an html file to contain the documentation, finished at least one route for each HTML method

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
+//
+// I was able to get https://gundamapi.herokuapp.com/
+//
+
 const express = require("express");
+const path = require('path');
 const Gundam = require("./model/gundam.model");
 const app = express();
 app.use(express.json());
@@ -6,20 +11,17 @@ app.use(express.json());
 const port = 4000;
 app.set("json spaces", 2);
 
-//GET ROOT
+//GET ROOT thanks: https://www.digitalocean.com/community/tutorials/use-expressjs-to-deliver-html-files
 app.get("/", (req, res) => {
-  res.send(`
-        <p>Go to: </p> 
-        <a href="http://localhost:4000/random">this page</a> 
-        <p>for a random gundam</p>
-    `);
+  res.sendFile(path.join(__dirname + '/documentation.html'), (err)=> {
+    if (err) console.log(err);
+  });
 });
 
 //GET Random Gundam : https://stackoverflow.com/questions/39277670/how-to-find-random-record-in-mongoose
 app.get("/random", (req, res) => {
-  count = Gundam.count().exec((err, total) => {
+  Gundam.count().exec((err, total) => {
     var randomCount = Math.floor(Math.random() * total);
-
     Gundam.findOne()
       .skip(randomCount)
       .exec((err, gundam) => {
@@ -29,22 +31,49 @@ app.get("/random", (req, res) => {
 });
 
 //GET Specific Gundam
-app.get("/gundam/:id", (req, res) => {
+app.get("/specific/:id", (req, res) => {
   Gundam.findById({ _id: req.params.id }).then((gundam) => {
     res.json(gundam);
   });
 });
 
-//app.post
+//GET Search: Mechanical Designer
+app.get("/designer/:id", (req, res) => {
+  Gundam.find({ "Mechanical Designer": { "$regex": `${req.params.id}`, "$options": "i" }}).then((gundam) => {
+    res.json(gundam);
+  });
+});
 
-//app.put
+//app.post
+app.post("/create", (req, res) => {
+  var newGundam = new Gundam({
+    "Official Name": req.body.name,
+    "Image": "www.imgur.com",
+  })
+
+  newGundam.save((err) => {
+    if (err) throw err;
+    else res.send('Gundam Created');
+  });
+});
+
+//app.put - overwrite this one plz: 5fa40ed328d8b8cefe3953c0
+app.put("/update/:id", (req, res) => {
+  Gundam.findById({ _id: req.params.id }).then((gundam) => {
+    gundam["Official Name"] = req.body.name
+    gundam.save((err) => {
+      if (err) throw err;
+      else res.send('Gundam Created');
+    });
+  });
+});
 
 //app.delete
-app.delete('/delete/:id', (req, res) => {
-    Gundam.findByIdAndDelete({ _id: req.params.id }).then(()=> {
-        console.log('Gundam Deleted')
-    })
-})
+app.delete("/delete/:id", (req, res) => {
+  Gundam.findByIdAndDelete({ _id: req.params.id }).then(() => {
+    res.send("Gundam Deleted");
+  });
+});
 
 app.listen(port, () => {
   console.log(`Listening on Port: ${port}`);
